575f2d2646287007b981c2c18549b95216e001a8
Michael Wright <m.wright92@icloud.com>
2022-04-06 21:29:52 -0700

    complete refactoring of old selector

---
+++ README.md
@@ -0,0 +1,32 @@
+# Language Suggester
+
+#### By Mike Wright
+
+#### A survey to help prospective programmers choose a language
+
+## Technologies Used
+
+* HTML
+* CSS
+* JavaScript
+ 
+ ## Description
+
+ This webpage is an exploration into JavaScript interactions and offers a light hearted survey to prospective coders with about three common coding languages.
+
+ ## Setup Instructions
+
+ * Clone repository from https://github.com/m-wright92/language-suggestor
+ * Navigate to the top level of the portfolio directory
+ * Open index.html in your web browser (it has only been tested in Google Chrome)
+
+ ## Known Bugs
+ None at time of publishing
+
+ 
+
+ ## License
+
+ MIT
+
+ Copyright (c) 03-27-2022 Mike Wright
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $("#submit").submit(function(event) {
+  $("form").submit(function(event) {
     event.preventDefault();
   })
 
@@ -8,12 +8,39 @@ $(document).ready(function () {
     $("#survey").removeClass("hidden");
   })
 
-  const name = $("#name-input").val();
-  const q1 = $("#color").val();
-  const q2 = $("#food").val();
-  const q3 = $("#flavor").val();
-  const q4 = $("#vacation").val();
-  const q5 = $("#image").val();
   
-  if
+  $("form").submit(function() {
+    const name = $("#user-name").val().trim();
+    const q1 = $("#color").val();
+    const q2 = $("#food").val();
+    const q3 = $("input:radio[name=flavor]:checked").val();
+    const q4 = $("input:radio[name=location]:checked").val();
+    const q5 = $("input:radio[name=tech]:checked").val();
+    surveyResults(name, q1, q2, q3, q4, q5);
+  })
+  
+  function surveyResults(name, q1, q2, q3, q4, q5) {
+    if (name === ""){
+      alert("You forgot your name!");
+    }else if(q1 === "Choose one" || q2 === "Choose one") {
+      alert("Missing an input(It's one of the dropdowns)")
+    }else if(q1 === "Red"){
+      if(q3 === "rocky" && q5 === "google") {
+        $("#survey").addClass("hidden");
+        $("#ruby, #restart").removeClass("hidden");
+        $(".name").text(name);
+      }
+    }else if(q1 === "Yellow") {
+      if(q2 === "Sushi" && q4 === "city") {
+        $("#survey").addClass("hidden");
+        $("#csharp, #restart").removeClass("hidden");
+        $(".name").text(name);
+      }
+    }else {
+      $("#survey").addClass("hidden");
+      $("#javascript, #restart").removeClass("hidden");
+      $(".name").text(name);
+    }
+  }
 })
+
